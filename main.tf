2a01b088b3ac7a55e893dabf16e384d80d49b861
Sourav Biswas <79691688+souravgit2021@users.noreply.github.com>
2026-04-21 21:08:52 +0530

    Add key_name and associate_public_ip_address to AWS instance

--- main.tf
+++ main.tf
@@ -2,6 +2,8 @@ resource "aws_instance" "example" {
      ami = "ami-0a1b6a02658659c2a"
      instance_type = "t2.micro"
      count = 3
+     key_name = "k8s"
+     associate_public_ip_address = true
      tags = {
        Environment = "prod"
      }
